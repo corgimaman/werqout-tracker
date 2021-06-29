82b011fd7b5b77b8077ac69f2c748556b2fa15ea
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-06-29 18:30:50 -0500

    updated server js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
@@ -14,7 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true, });
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
