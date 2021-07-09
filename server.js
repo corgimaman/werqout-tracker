8ce52e05ffb552ff399cc6b1bbf5574fa3383485
corgimaman <78245727+corgimaman@users.noreply.github.com>
2021-07-08 21:20:00 -0500

    removed env

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
